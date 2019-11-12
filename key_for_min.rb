164aa8496b31eeab5d31595c87b6ee4186595012
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-12 19:41:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,6 +3,7 @@
 require "pry"
 def key_for_min_value(name_hash)
 lowest_value = ""
+lowest_key = ""
 name_hash.each do |key,value|
   if lowest_value == "" || value < lowest_value
     lowest_value = value
